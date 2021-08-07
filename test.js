6ca564dc73c69746e3d2f5e5ac2a7e4f4ac08174
ndrslmpk <andreas.lempik@gmail.com>
2021-08-07 19:57:33 +0200

    try truffle's react box as template

---
+++ test.js
@@ -0,0 +1,19 @@
+let Lc = artifacts.require("./Lcfactory.sol");
+
+let lcInstance;
+
+conract('Letter of Credit', function(accounts){
+    //accounts[0] is the default account
+    
+    /*
+              TEST CASE 1
+    */
+    it("Contract deployment", function(){
+        return Lc.deployed().then(function (instance) {
+            lcInstance = instance;
+            assert(lcInstance !== undefined, 'Letter of Credit Contract should be defined');
+        });
+    });
+
+
+})
