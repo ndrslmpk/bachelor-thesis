6f8b6e7c3ecaf35e8174256185ff770f782096f7
ndrslmpk <andreas.lempik@gmail.com>
2021-08-14 20:54:31 +0200

    adding animations and fonts to Issuance Component

--- eth-detrafin-test-truffle-box/client/src/App.js
+++ eth-detrafin-test-truffle-box/client/src/App.js
@@ -76,17 +76,33 @@ class App extends Component {
               <img src={PenginLogo} alt="Pengin Logo" />
             </div>
             <div className="flex flex-col w-full">
-              <h1 className="flex w-full text-xl text-left text-white">Issue your Letter of Credit</h1>
-              <p className="flex w-full text-left text-white">For what are you waiting for?</p>
+              <h1 className="flex w-full text-xl text-left text-white font-mono font">Issue your Letter of Credit</h1>
+              <p className="flex w-full text-left text-white">What are you waiting for?</p>
             </div>
           </div>
-          <div className="flex flex-col">
-            <form action="" method="post">
-              <input type="text" name="" id="" placeholder="Your company name" className="w-1/2 mx-5 my-1 border-b bg-transparent border-primary border-solid text-gray-300 focus:text-primarydarkdark focus:placeholder-primary placeholder-gray-500 placeholder-opacity-50" />
-              <input type="text" name="" id="" placeholder="Your company ETH address" className="w-1/2 mx-5 my-1 border-b bg-transparent border-primary border-solid text-gray-300 focus:text-primarydarkdark focus:placeholder-primary placeholder-gray-500 placeholder-opacity-50" />
-              <input type="text" name="" id="" placeholder="Your contract subject" className="w-1/2 mx-5 my-1 border-b bg-transparent border-primary border-solid text-gray-300 focus:text-primarydarkdark focus:placeholder-primary placeholder-gray-500 placeholder-opacity-50" />
-              <input type="number" name="" id="" placeholder="Your contract value" className="w-1/2 mx-5 my-1 border-b bg-transparent border-primary border-solid text-gray-300 focus:text-primarydarkdark focus:placeholder-primary placeholder-gray-500 placeholder-opacity-50" />
-            </form>
+          <div id="animatedBlock" className="">
+            <div className="flex flex-col    border-2 border-white border-dashed     mt-10 p-10 rounded-2xl shadow-lg">
+              <h1 className="flex w-full justify-center mb-5     text-white text-xl font-mono font-bold">Define your Contract's (NFT) parameters</h1>
+              <form action="" method="post">
+                <input type="text" name="" id="" placeholder="Your company name" className="font-mono w-1/2 mx-5 my-1 border-b bg-transparent border-primary border-solid text-gray-300 focus:text-primarydarkdark focus:placeholder-primary placeholder-gray-500 placeholder-opacity-50" />
+                <input type="text" name="" id="" placeholder="Your company ETH address" className="font-mono w-1/2 mx-5 my-1 border-b bg-transparent border-primary border-solid text-gray-300 focus:text-primarydarkdark focus:placeholder-primary placeholder-gray-500 placeholder-opacity-50" />
+                <input type="text" name="" id="" placeholder="Your contract subject" className="font-mono w-1/2 mx-5 my-1 border-b bg-transparent border-primary border-solid text-gray-300 focus:text-primarydarkdark focus:placeholder-primary placeholder-gray-500 placeholder-opacity-50" />
+                <input type="number" name="" id="" placeholder="Your contract value" className="font-mono w-1/2 mx-5 my-1 border-b bg-transparent border-primary border-solid text-gray-300 focus:text-primarydarkdark focus:placeholder-primary placeholder-gray-500 placeholder-opacity-50" />
+                <div className="flex flex-row items-end mt-10">
+                  <div className="flex w-1/2 mx-2 justify-end">
+                    <button className="w-1/3 bg-transparent bg-primarydark hover:bg-primarydark text-primarydarkdark font-semibold hover:text-white py-2 px-4 border border-primarydark hover:border-transparent rounded        transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+                      Create
+                    </button>
+                  </div>
+                  <div className="flex w-1/2 mx-2 justify-start">
+                    <button className="w-1/3 bg-transparent hover:bg-gray-500 text-gray-700 font-semibold hover:text-white py-2 px-4 border border-gray-700 hover:border-transparent rounded" disabled>
+                      Reset
+                    </button>
+                  </div>
+  
+                </div>
+              </form>
+            </div>
           </div>
         </div>
 
