37bf1498129d7fabc302fc02e0fae8eb03774e1f
ndrslmpk <andreas.lempik@gmail.com>
2021-09-23 22:05:55 +0200

    added ERC721 Interface implementation

--- eth-detrafin-test-truffle-box/contracts/OffchainContract.sol
+++ eth-detrafin-test-truffle-box/contracts/OffchainContract.sol
@@ -24,13 +24,13 @@ contract OffchainContract is IERC721 {
     bytes32[] name;
     string symbol;
     uint256 value;
-    address owner;
+    address contractOwner;
 
     constructor(bytes32[] memory _name, uint256 _value) {
         name = _name;
         value = _value; // Contract Value
         uint256 tokenId = CONTRACT_SERIAL_NUMBER++; //  sets the new tokenId and increments the SERIAL_NUMBER for the next contract
-        owner = msg.sender;
+        contractOwner = msg.sender;
     }
 
     function balanceOf(address _owner)
@@ -39,7 +39,9 @@ contract OffchainContract is IERC721 {
         override
         returns (uint256 balance)
     {
-        return ownersContractList[owner].length;
+        require(msg.sender != address(0));
+
+        return ownersContractList[contractOwner].length;
     }
 
     function ownerOf(uint256 tokenId)
@@ -48,6 +50,87 @@ contract OffchainContract is IERC721 {
         override
         returns (address _owner)
     {
+        require(msg.sender != address(0));
         require(contractsOwnerList[tokenId].length == 0);
+        require(tokenId >= 0);
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) external override {
+        require(from != address(0));
+        require(to >= address(0));
+        require(tokenId > 0);
+        //requiring msg.sender to be the same person as the from transfer
+        // Todo: It needs to be allowed to transfer tokens on the behalf of somebody else
+        require(msg.sender == from);
+        // require(msg.sender == contractsOwnerList[tokenId]); //checks whether the requested tokenId has an owner with the sender's address
+
+        Transfer(from, to, tokenId);
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) external override {}
+
+    function approve(address to, uint256 tokenId) external override {
+        require(tokenId != 0);
+    }
+
+    function getApproved(uint256 tokenId)
+        external
+        view
+        override
+        returns (address operator)
+    {
+        require(msg.sender != operator);
+    }
+
+    function setApprovalForAll(address operator, bool _approved)
+        external
+        override
+    {}
+
+    function isApprovedForAll(address owner, address operator)
+        external
+        view
+        override
+        returns (bool)
+    {
+        require(msg.sender != owner);
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes calldata data
+    ) external override {
+        require(from != address(0), "from cannot be the zero address");
+        require(to != address(0), "from cannot be the zero address");
+        require(contractsOwnerList[tokenId] == from);
+        require(checkOwnership(tokenId, from));
+        Transfer(from, to, tokenId);
+    }
+
+    function checkOwnership(uint256 tokenId, address checkOwner)
+        public
+        returns (bool)
+    {
+        address[] memory _owners = contractsOwnerList[tokenId];
+
+        // Checks whether the
+        for (uint256 i = 0; i < contractsOwnerList[tokenId].length; i++) {
+            address _addr = _owners[i];
+            if (_addr == checkOwner) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 }
