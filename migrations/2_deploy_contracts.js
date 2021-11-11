d60199b4c68e8bf16768705ba9810c2128bf358b
ndrslmpk <andreas.lempik@gmail.com>
2021-11-11 23:45:23 +0100

    modified 2. migration to add Escrow.sol and Lc.sol to the testnet

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var Escrow = artifacts.require("../contracts/Escrow.sol");
+var Lc = artifacts.require("../contracts/Lc.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+module.exports = function (deployer) {
+  deployer.deploy(Lc);
+  deployer.deploy(Escrow);
 };
