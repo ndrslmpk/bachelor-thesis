22c930358b0da09e611482cc88e8a27490afb1ef
ndrslmpk <andreas.lempik@gmail.com>
2021-11-14 00:19:28 +0100

    stashing changes :sparkles:

--- contracts/Lc.sol
+++ contracts/Lc.sol
@@ -1,2 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
+
+contract Lc {
+
+  mapping (uint => address) lcIdRegister;
+  uint256 validityTime;
+
+  constructor(uint256 _validityPeriod) {
+    validityTime = block.timestamp + _validityPeriod;
+  }
+}
--- test/TestSimpleStorage.sol
+++ test/TestSimpleStorage.sol
@@ -1,8 +1,9 @@
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity ^0.8.10;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/SimpleStorage.sol";
+
 
 contract TestSimpleStorage {
 
