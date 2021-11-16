17cc6b9e1bdf33472e8b0dd1aec4ec511cecd468
ndrslmpk <andreas.lempik@gmail.com>
2021-11-16 12:55:41 +0100

    :sparkles: introduced more structure into testing and added state testing; fixed :bug:

--- test/TestEscrow.js
+++ test/TestEscrow.js
@@ -10,36 +10,44 @@ contract("Testing Escrow SC", (accounts) => {
   const exporter = accounts[1];
   const carrier = accounts[2];
 
-  it("ESCROW: Initial escrow contract test", async () => {
-    const escrow = await TestEscrow.deployed();
-    const escrowValue = await escrow.value();
-    assert.equal(escrowValue, 10, "100 was not in the first account");
-  });
+  describe("Initialization Testing", async () => {
+    it("Contract value is according", async () => {
+      const escrow = await TestEscrow.deployed();
+      const escrowValue = await escrow.value();
+      assert.equal(escrowValue, 10, "100 was not in the first account");
+    });
 
-  it("ESCROW: Constructor test", async () => {
-    const escrow = await TestEscrow.deployed();
-    console.log(escrow); //checking if contract was properly deployed
+    it("Accounts are initialized properly", async () => {
+      const escrow = await TestEscrow.deployed();
 
-    // await escrow.new(carrier, exporter, 10, { from: accounts[0] });
-    assert.equal(
-      await escrow.owner(),
-      importer,
-      "The importer is not the contract owner, which is accounts[0]."
-    );
-    assert.equal(
-      await escrow.exporter(),
-      exporter,
-      "The exporter is not accounts[1]."
-    );
-    assert.equal(
-      await escrow.carrier(),
-      carrier,
-      "The carrier is not accounts[2]."
-    );
-    assert.equal(
-      await escrow.value(),
-      10,
-      "THe value of the ESCROW account is not 10."
-    );
+      assert.equal(
+        await escrow.owner(),
+        importer,
+        "The importer is not the contract owner, which is accounts[0]."
+      );
+      assert.equal(
+        await escrow.exporter(),
+        exporter,
+        "The exporter is not accounts[1]."
+      );
+      assert.equal(
+        await escrow.carrier(),
+        carrier,
+        "The carrier is not accounts[2]."
+      );
+      assert.equal(
+        await escrow.value(),
+        10,
+        "THe value of the ESCROW account is not 10."
+      );
+    });
+    it("State is initialized properly", async () => {
+      const escrow = await TestEscrow.deployed();
+      assert.equal(
+        await escrow.state(),
+        TestEscrow.State.waitingForDeposit,
+        "Initital State of Escrow should be waitingForDeposit"
+      );
+    });
   });
 });
