86270601593f52fa8888532453b964681e07937f
ndrslmpk <andreas.lempik@gmail.com>
2021-11-18 23:33:15 +0100

    adding state variable to track status of agreement

--- contracts/Lc.sol
+++ contracts/Lc.sol
@@ -1,14 +1,47 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
 
+/// @title Non-fungible Token representing an Lc Contract
+/// @author Ndrslmpk
+/// @notice Explain to an end user what this does
+/// @dev Explain to a developer any extra details
 contract Lc {
 
+  address payable public importer;
+  address payable public exporter;
+  address public carrier;
+  State public state;
+  bool public importerAgreed = false;
+  bool public exporterAgreed = false;
+
   mapping (uint => address) numberToOwner;
+
+  /// @notice Introduces ownership of explicit token ids
+  /// @dev Ownership is determined by an array of tokens indexes (tokenIDs)
+  mapping (address => uint256[]) public ownedContracts;
+
+  /// @notice Lookup for tokenOwner
+  /// @dev Ownership is determined by an array of tokens indexes (tokenIDs)
+  mapping (uint256 => address) public contractOwner;
+
+/// @notice Enum allows to define certain states of the lc to determine which actions will be allowed and avoided
+/// @dev opened is initiated after calling the constructor
+/// @dev confirmed is the state after the signing, thus agreeing, of both parties
+/// @dev closed is the state after each party got their needed asset (e.g. product and money)
+  enum State {
+    opened, confirmed, closed
+  }
+
+  uint256[] internal allContracts;
   uint256 validityTime;
 
+  ///
+  /// Assumptions: Importer is the only party calling the LC constructor
   constructor() {
     uint256 _validityPeriod = 90 days;
     validityTime = block.timestamp + _validityPeriod;
+    state = State.opened;
+    importerAgreed = true; //assumption that importer always initializes contract 
   }
 
   
