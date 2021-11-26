081ee1f41a2c2c41a8010051e2dabef7da661331
ndrslmpk <andreas.lempik@gmail.com>
2021-11-26 02:35:12 +0100

    minor changes

--- client/src/components/wallet/WalletConnector.jsx
+++ client/src/components/wallet/WalletConnector.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import { useWeb3React } from "@web3-react/core";
 import { injected } from "./Connectors";
 
+/* 
+    Connectors are stand-alone packages that interface with web3-react and 
+    manage connections to a single type of Ethereum node or wallet. To give an example
+ */
+
 export const WalletConnector = ({ className }) => {
   /*
-      active: is a wallet actively connected right now?
-      account: the blockchain address that is connected
-      library: this is either web3 or ethers, depending what you passed in
+      active: (BOOL) is a wallet actively connected right now
+      account: (address) the blockchain address that is connected
+      library: () this is either web3 or ethers, depending what you passed in
       connector: the current connector. So, when we connect it will be the injected connector in this example
-      activate: the method to connect to a wallet
-      deactivate: the method to disconnect from a wallet
+      activate: (wallet) the method to connect to a wallet
+      deactivate: () the method to disconnect from a wallet
   */
   // eslint-disable-next-line no-unused-vars
   const { active, account, library, connector, activate, deactivate } =
